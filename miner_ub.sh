dbefa0d8cf81ee9318c60e354be4aed0a4f83d55
rommie7 <39836571+rommie7@users.noreply.github.com>
2018-06-01 19:47:28 +0300

    fix

--- miner_ub.sh
+++ miner_ub.sh
@@ -4,5 +4,5 @@ NUM_CORE='nproc'
 echo "starting aquaminer" | logger
 for i in $(seq 1 "$NUM_CORE"); do
   echo launching worker $i
-  /home/ubuntu/aquaminer-0.4-linux-amd64 -F "http://172.16.21.100:8543" -t 1 -r 1s
+  /home/ubuntu/aquaminer-0.4-linux-amd64 -F "http://172.16.21.100:8543" -t 1 -r 1s 2>>"/home/ubuntu/log2.log" 1>>"/home/ubuntu/log1.log" &
 done
